dc07367fd53ea3764489ae9bc34f6934a176e7f6
Mik-Grzeg <mikolaj.grzegorzek@protonmail.com>
2022-09-17 20:15:40 +0200

    Refactor dummy importer

--- dummy-importer/producer.py
+++ dummy-importer/producer.py
@@ -1,3 +1,4 @@
+from base64 import encode
 import json
 import time
 import zmq
@@ -17,22 +18,24 @@ def producer():
 
 
     while True:
+        src_ip = ".".join(map(str, (random.randint(0, 255) for _ in range(4))))
+        dst_ip = ".".join(map(str, (random.randint(0, 255) for _ in range(4))))
         msg = dict(
             OUT_BYTES = random.randint(30, 1000),
             OUT_PKTS = random.randint(0, 10),
             L4_DST_PORT = random.randint(1, 33333),
-            IPV4_DST_ADDR = ".".join(map(str, (random.randint(0, 255) for _ in range(4)))),
-            IPV4_SRC_ADDR = ".".join(map(str, (random.randint(0, 255) for _ in range(4)))),
+            IPV4_DST_ADDR = src_ip,
+            IPV4_SRC_ADDR = dst_ip,
             PROTOCOL =  random.randint(0, 20),
             IN_BYTES = random.randint(50, 500),
             IN_PKTS = random.randint(0, 10),
             L7_PROTO = "Unknown",
             TCP_FLAGS = random.randint(0, 20),
-            FLOW_DURATION_MILLISECONDS = random.randint(0, 100000)
+            FLOW_DURATION_MILLISECONDS = random.randint(0, 100000),
         )
 
         print(f"{msg}")
-        work_message = [b"flows", bytes(f'flow {json.dumps(msg)}', encoding='utf-8')]
+        work_message = [b"flows", bytes(json.dumps(msg), encoding='utf-8')]
         zmq_socket.send_multipart(work_message)
         print(f"Sent message: {work_message}")
         time.sleep(random.uniform(0,2))
