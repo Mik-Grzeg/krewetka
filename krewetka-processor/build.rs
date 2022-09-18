96feadfb4f2cdaaf762b67c11087ebf986af4943
Mik-Grzeg <mikolaj.grzegorzek@protonmail.com>
2022-09-18 14:30:33 +0200

    Instead of using workspaces I splitted those into 2 packages
    * created separate CI jobs for processor and collector
    * created separate dockerfiles for processor and collector

--- krewetka-processor/build.rs
+++ krewetka-processor/build.rs
@@ -2,7 +2,7 @@ fn main() {
 
     let proto_path = match std::env::var("PROTO_PATH") {
         Ok(val) => val,
-        Err(_) => "../proto/flow.proto".to_string(),
+        Err(_) => "proto/flow.proto".to_string(),
     };
 
     tonic_build::compile_protos(&proto_path)
