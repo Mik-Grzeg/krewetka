493d3646907da9eda7ff796e5ccc1e85f3733855
Mikołaj Grzegorzek <58633804+Mik-Grzeg@users.noreply.github.com>
2022-10-15 21:54:05 +0200

    Test arm build with cross from main branch (#9)

--- krewetka-collector/prebuild.sh
+++ krewetka-collector/prebuild.sh
@@ -1,13 +1,3 @@
 dpkg --add-architecture armhf
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -yy libzmq3-dev:armhf libssl-dev:armhf wget cmake libprotobuf-dev
-wget https://github.com/protocolbuffers/protobuf/releases/download/v21.7/protobuf-all-21.7.tar.gz
-tar -xzf protobuf-all-21.7.tar.gz
-
-cd protobuf-21.7
-cmake .
-cmake --build . --parallel 10
-cmake --install .
-ldconfig
-
-protoc --version
+DEBIAN_FRONTEND=noninteractive apt-get install -yy libzmq3-dev:armhf libssl-dev:armhf libprotobuf-dev protobuf-compiler
