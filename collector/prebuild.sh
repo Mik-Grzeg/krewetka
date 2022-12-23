c952943a2a24f6f38bcd805e668d336e12fefd6e
Mik-Grzeg <mikolaj.grzegorzek@protonmail.com>
2022-12-23 19:20:57 +0100

    Text ci

--- collector/prebuild.sh
+++ collector/prebuild.sh
@@ -1,3 +1,3 @@
 dpkg --add-architecture armhf
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -yy libzmq3-dev:armhf libssl-dev:armhf libprotobuf-dev protobuf-compiler
+DEBIAN_FRONTEND=noninteractive apt-get install -yy libzmq3-dev:armhf libssl-dev:armhf libprotobuf-dev protobuf-compiler zlib1g zlib1g-dev
